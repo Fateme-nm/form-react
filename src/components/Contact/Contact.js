72bb9c93565af8cf5753c93bf05aa833215ae484
fatemeh <fatemehnm1378@gmail.com>
2022-03-03 15:36:35 +0330

    modify

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 
 class Contact extends Component {
     render() {
-        return (
-                Object.values(this.props.itemsContact).map(value => <td>{value}</td>)
-        );
+        return Object.values(this.props.itemsContact).map(value => <td>{value}</td>)
     }
 }
 
