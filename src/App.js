8a3dfbc38421b3f54b7e013f3b0d363a58be0cfb
fatemeh <fatemehnm1378@gmail.com>
2022-03-03 16:17:58 +0330

    class App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React, { Component } from 'react';
+import {Container, Row, Col} from 'react-bootstrap'
 import FormContact from './components/FormContact/FormContact';
 
-function App() {
-  return (
-    <div className="App">
-      <FormContact />
-    </div>
-  );
+class App extends Component{
+  constructor(prop) {
+    super(prop)
+    this.state = {
+      registeredContacts : []
+    }
+  }
+  
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col><FormContact /></Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
