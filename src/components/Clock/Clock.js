42ebfdc54196d20c5ad7cbe0bdbfbeb23b357651
fatemeh <fatemehnm1378@gmail.com>
2022-03-10 17:05:29 +0330

    change to functional component

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,48 +1,62 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-class Clock extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            time : new Date,
-            color: '#000'
+function Clock () {
+    // constounctor (props) {
+    //     super(props)
+    //     this.state = {
+    //         time: new Date,
+    //         color: '#000'
+    //     }
+    // }
+    
+    // componentDidMount() {
+    //     this.timeInterval = setInterval(() => this.setTime(), 1000)
+    // }
+
+    // componentDidUpdate(prevProp, prevState) {
+    //     if(this.state.time !== prevState.time ) {
+    //         this.changeColor()
+    //     }
+    // }
+
+    // componentWillUnmount() {
+    //     if (this.timeInterval !== null) {
+    //         clearInterval(this.interval)
+    //     }
+    // }
+
+    let [time, timeState] = useState(new Date)
+    let [color, colorState] = useState('#000')
+    let timeInterval = null
+
+    useEffect (() => {
+        timeInterval = setInterval(() => timeState(new Date), 1000)
+        return () => {
+            if (timeInterval) {
+                clearInterval(timeInterval)
+            }
         }
-    }
+    }, [])
 
-    componentDidMount() {
-        this.timeInterval = setInterval(() => this.setTime(), 1000)
-    }
+    useEffect (() => {
+        changeColor()
+    }, [time])
 
-    componentDidUpdate(prevProp, prevState) {
-        if(this.state.time !== prevState.time ) {
-            this.changeColor()
-        }
-    }
-
-    componentWillUnmount() {
-        if (this.timeInterval !== null) {
-            clearInterval(this.interval)
-        }
-    }
-
-    setTime = () => this.setState({time: new Date})
-
-    changeColor = () => {
+    function changeColor () {
         let newColor = "#" + Math.floor(Math.random()*16777215).toString(16)
-        this.setState({color: newColor})
+        colorState(newColor)
     }
 
-    render() {
-        let newStyle = {
-            color: this.state.color, 
-            fontSize:'1.2rem'
-        }
-        return (
-            <div>
-                <div style={newStyle}>{this.state.time.toLocaleTimeString()}</div>
-            </div>
-        );
+    let newStyle = {
+        color: color, 
+        fontSize:'1.2rem'
     }
+
+    return (
+        <div>
+            <div style={newStyle}>{time.toLocaleTimeString()}</div>
+        </div>
+    );
 }
 
 export default Clock;
